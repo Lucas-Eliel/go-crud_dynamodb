275770372607a567af58e34734192dba15b2211e
lucaseliel <lucaseliel.business@gmail.com>
2023-05-19 22:03:56 -0300

    Criando testes unitários usando gomock

---
+++ internal/services/user_service_test.go
@@ -0,0 +1,28 @@
+package services_test
+
+import (
+	"testing"
+
+	mock_repositories "crud_dynamodb/internal/mocks"
+	"crud_dynamodb/internal/models"
+	services "crud_dynamodb/internal/services"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetUsuarios(t *testing.T) {
+	//Criar cenário
+	ctrl := gomock.NewController(t)
+	mockRepository := mock_repositories.NewMockRepository(ctrl)
+	var users []models.User
+	users = append(users, models.User{ID: "1", Nome: "João"}, models.User{ID: "2", Nome: "Pedro"})
+	mockRepository.EXPECT().FindAll().Return(users)
+
+	//Chamar o metodo a ser testado
+	serv := services.NewService(mockRepository)
+	resp := serv.GetUsuarios()
+
+	//Realizar as verficações
+	assert.NotEmpty(t, resp)
+}
